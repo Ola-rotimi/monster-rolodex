c2f5a46ecc0e5228878eecc728939fd1c90e9d45
Olarotimi Adamson <olarotimiadamson@yahoo.com>
2023-01-26 10:39:31 +0100

    card list updated

--- src/components/card-list/card-list.component.js
+++ src/components/card-list/card-list.component.js
@@ -1,14 +1,25 @@
 import React from "react";
+import "./card-list.styles.css";
 
 function CardList(props) {
-    console.log(props)
-    const { monsters } = props;
-    console.log('render CardList');
+  console.log(props);
+  const { monsters } = props;
+  console.log("render CardList");
   return (
     <div className="card-list">
-      {monsters.map((monster) => (
-        <h1 key={monster.id}>{monster.name}</h1>
-      ))}
+      {monsters.map((monster) => {
+        const { id, name, email } = monster;
+        return (
+          <div className="card-container" key={id}>
+            <img
+              src={`https://robohash.org/${id}?set=set2&size=180x180`}
+              alt={`monster ${name}`}
+            />
+            <h1>{name}</h1>
+            <p>{email}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
